b01d98bc4ee10ef4fd0dcc4bd7260945538b404f
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2013-08-31 18:35:53 -0700

    Existing tables no longer bother migrations
    And also, updated the database API. There's no elegant way to open
    connections, and then to close them, unfortunately. At least so far.

--- migrations/001-devices.js
+++ migrations/001-devices.js
@@ -6,18 +6,25 @@ var utils = require('../lib/utils');
 var async = require('async');
 
 var runQuery = utils.database.query.bind(utils.database);
+var onDoneQueries = function (next) {
+  return function (err) {
+    utils.database.end();
+    if (err) throw err;
+    next();
+  };
+};
 
 exports.up = function(next) {
 
   var devicesQuery =
-  'CREATE TABLE `devices` ( \
-    `id` int(11) unsigned NOT NULL, \
+  'CREATE TABLE IF NOT EXISTS `devices` ( \
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT, \
     `device_number` int(11) unsigned NOT NULL, \
     PRIMARY KEY (`id`) \
   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;';
   
   var energyConsumptionsQuery =
-  'CREATE TABLE `energy_consumption` ( \
+  'CREATE TABLE IF NOT EXISTS `energy_consumption` ( \
     `id` int(11) unsigned NOT NULL AUTO_INCREMENT, \
     `device_id` int(11) unsigned NOT NULL, \
     `kw` float NOT NULL, \
@@ -28,7 +35,7 @@ exports.up = function(next) {
   ) ENGINE=InnoDB AUTO_INCREMENT=93 DEFAULT CHARSET=latin1;';
   
   var hourlyTotalsQuery =
-  'CREATE TABLE `hourly_totals` ( \
+  'CREATE TABLE IF NOT EXISTS `hourly_totals` ( \
     `id` int(11) unsigned NOT NULL AUTO_INCREMENT, \
     `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \
     `start_kwh` float NOT NULL, \
@@ -38,8 +45,8 @@ exports.up = function(next) {
     KEY `device_id` (`device_id`) \
   ) ENGINE=InnoDB DEFAULT CHARSET=latin1;';
 
-  var minutelyTotalsQUery =
-  'CREATE TABLE `per_minute_totals` ( \
+  var perMinuteTotalsQuery =
+  'CREATE TABLE IF NOT EXISTS `per_minute_totals` ( \
     `id` int(11) unsigned NOT NULL AUTO_INCREMENT, \
     `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \
     `start_kwh` float NOT NULL, \
@@ -52,21 +59,19 @@ exports.up = function(next) {
     devicesQuery,
     energyConsumptionsQuery,
     hourlyTotalsQuery,
-    minutelyTotalsQUery
+    perMinuteTotalsQuery
   ];
 
-  async.eachSeries(queries, runQuery, function (err) {
-    if (err) {
-      return function () {
-        console.error(err);
-        next(err);
-      };
-    }
-    next();
-  });
+  async.eachSeries(queries, runQuery, onDoneQueries(next));
 };
 
 exports.down = function(next){
-  var query = 'DROP TABLE `devices`';
-  utils.database.query(query, next);
+  var queries = [
+    'DROP TABLE `devices`;',
+    'DROP TABLE `energy_consumption`;',
+    'DROP TABLE `hourly_totals`;',
+    'DROP TABLE `per_minute_totals`;'
+  ];
+
+  async.eachSeries(queries, runQuery, onDoneQueries(next));
 };
