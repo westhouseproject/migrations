46506d1c4f931f1dd64df50e9238174096fe83f1
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2014-06-10 22:40:13 -0700

    Back to square one.

--- up/1.sql
+++ up/1.sql
@@ -1,6 +1,102 @@
-CREATE TABLE `energy_consumer_devices` (
+CREATE TABLE `data_points` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `device_id` varchar(255) NOT NULL DEFAULT '',
-  `name` varchar(255) DEFAULT NULL,
+  `device_id` int(11) unsigned NOT NULL,
+  `value` float NOT NULL,
+  `running_total` float NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `device_id` (`device_id`),
+  CONSTRAINT `data_points_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=74341 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `data_points_1d` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `device_id` int(11) unsigned NOT NULL,
+  `mean` float NOT NULL,
+  `running_total` float NOT NULL,
+  `time` datetime NOT NULL,
+  `sum` float NOT NULL,
+  `min` float NOT NULL,
+  `max` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `device_id` (`device_id`),
+  CONSTRAINT `data_points_1d_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `data_points_1h` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `device_id` int(11) unsigned NOT NULL,
+  `mean` float NOT NULL,
+  `running_total` float NOT NULL,
+  `time` datetime NOT NULL,
+  `sum` float NOT NULL,
+  `min` float NOT NULL,
+  `max` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `device_id` (`device_id`),
+  CONSTRAINT `data_points_1h_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=169 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `data_points_1m` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `device_id` int(11) unsigned NOT NULL,
+  `mean` float NOT NULL,
+  `running_total` float NOT NULL,
+  `time` datetime NOT NULL,
+  `sum` float NOT NULL,
+  `min` float NOT NULL,
+  `max` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `device_id` (`device_id`),
+  CONSTRAINT `data_points_1m_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3109 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `data_points_1mo` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `device_id` int(11) unsigned NOT NULL,
+  `mean` float NOT NULL,
+  `running_total` float NOT NULL,
+  `time` datetime NOT NULL,
+  `sum` float NOT NULL,
+  `min` float NOT NULL,
+  `max` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `device_id` (`device_id`),
+  CONSTRAINT `data_points_1mo_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `data_points_1w` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `device_id` int(11) unsigned NOT NULL,
+  `mean` float NOT NULL,
+  `running_total` float NOT NULL,
+  `time` datetime NOT NULL,
+  `sum` float NOT NULL,
+  `min` float NOT NULL,
+  `max` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `device_id` (`device_id`),
+  CONSTRAINT `data_points_1w_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `data_points_1y` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `device_id` int(11) unsigned NOT NULL,
+  `mean` float NOT NULL,
+  `running_total` float NOT NULL,
+  `time` datetime NOT NULL,
+  `sum` float NOT NULL,
+  `min` float NOT NULL,
+  `max` float NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `device_id` (`device_id`),
+  CONSTRAINT `data_points_1y_ibfk_1` FOREIGN KEY (`device_id`) REFERENCES `devices` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `devices` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `real_device_id` varchar(256) NOT NULL DEFAULT '',
+  `name` varchar(256) DEFAULT NULL,
+  `type` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=85 DEFAULT CHARSET=latin1;
